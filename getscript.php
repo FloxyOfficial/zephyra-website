8aee630b1ea0fbb4131a7ba1ff9d843033d7334e
FloxyOfficial <playgoodman1@gmail.com>
2025-04-12 20:32:41 +0200

    Update getscript.php

--- getscript.php
+++ getscript.php
@@ -1,7 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: text/plain");
+
 $scriptFile = __DIR__ . '/Protected_4174647752688388.txt';
 if (file_exists($scriptFile)) {
-    header('Content-Type: text/plain');
     readfile($scriptFile);
 } else {
     http_response_code(404);
